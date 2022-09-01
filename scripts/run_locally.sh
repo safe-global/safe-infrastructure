64df8ee5c97f018b3f892d3f5b2f078e9da6fc02
Hector Gómez Varela <hector.gomez.varela@gmail.com>
2022-09-01 18:06:08 +0200

    Update run_locally.sh script

--- scripts/run_locally.sh
+++ scripts/run_locally.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
+set -e
+
 echo "==> $(date +%H:%M:%S) ==> Setting up environment variables from sample file..."
-TEMP=temp.env
-cp ../.env.sample $TEMP
+TEMP=temp.env \
+  && cp ../.env.sample $TEMP \
+  || exit
 
 echo "==> $(date +%H:%M:%S) ==> Using http://polygon-rpc.com as default RPC node"
-sed -i '' 's|http://url.to.node|https://polygon-rpc.com|g' $TEMP
-mv $TEMP ../.env
+sed -i '' 's|http://url.to.node|https://polygon-rpc.com|g' $TEMP \
+  && mv $TEMP ../.env \
+  || exit
 
 echo "==> $(date +%H:%M:%S) ==> Starting up environment containers..."
 docker compose up -d \
   && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Config Service..." \
   && docker compose exec cfg-web python src/manage.py createsuperuser \
   && echo "==> $(date +%H:%M:%S) ==> Creating super-user for Safe Transaction Service..." \
-  && docker compose exec txs-web python manage.py createsuperuser
+  && docker compose exec txs-web python manage.py createsuperuser \
+  || exit
 
-echo "==> $(date +%H:%M:%S) ==> Adding ChainInfo..."
+echo "==> $(date +%H:%M:%S) ==> All set! You may want to add a ChainInfo into the Config service. Please use the link below to fill its data: "
